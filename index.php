d4fe22b1b3ec595313e44c4993bc18617036c839
Dylan Bannon <dylanrb123@gmail.com>
2016-09-19 15:46:46 -0400

    changed shirt image

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
             <div class="panel-body" style="color: black">
                 <div class="media">
                     <div class="media-body">
-                        <h1>Fall Fundraiser</h1>
+                        <h1 class="media-heading">Fall Fundraiser</h1>
                         <p>
                             We're raising money to pay for our fall season's equipment/coaching expenses. We'll be
                             racing in the Head of the Genesee Regatta on October 8th, and the top 10 donations will get
@@ -16,7 +16,7 @@
                         </p>
                     </div>
                     <div class="media-right">
-                        <img class="media-object" src="assets/images/shirt.png" style="height: 150px;">
+                        <img class="media-object" src="assets/images/shirt.png" style="height: 150px; margin-top: 70px;">
                     </div>
                 </div>
             </div>
